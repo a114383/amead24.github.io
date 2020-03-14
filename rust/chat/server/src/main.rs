f332159ba61b0b41622dea68e599b7daa5c23967
anthony mead <amead2@kent.edu>
2020-03-13 22:20:19 -0400

    stash

--- rust/chat/server/src/main.rs
+++ rust/chat/server/src/main.rs
@@ -1,10 +1,17 @@
-use std::collections::HashMap;
-use std::env;
+// use std::collections::HashMap;
+// use std::env;
 use std::io::{ErrorKind, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::mpsc;
 use std::thread;
 
+// TODO: Make into nicer CLI
+
+//let args: Vec<String> = env::args().collect();
+//let pargs: HashMap<&str, &str> = parse_args(args);
+
+//let URL: &str = format!("127.0.0.1:{}", pargs.entry("--port").or_default("8888"));
+//let MSIZE: usize = pargs.entry("--buff").or_default(128).parse().unwrap();
 
 //fn parse_args(args: Vec<String>) -> HashMap<&'str, String> {
 //    let mut pargs: HashMap<&str, String> = HashMap::new();
@@ -29,67 +36,30 @@ const MSIZE: usize = 32;
 
 
 fn sleep() {
-    thread::sleep_ms(100);
+    thread::sleep(std::time::Duration::from_millis(100));
 }
 
 
 fn main() {
-    //let args: Vec<String> = env::args().collect();
-    //let pargs: HashMap<&str, &str> = parse_args(args);
-
-    //let URL: &str = format!("127.0.0.1:{}", pargs.entry("--port").or_default("8888"));
-    //let MSIZE: usize = pargs.entry("--buff").or_default(128).parse().unwrap();
-
     let listener = TcpListener::bind(URL).expect("Could not bind");
     listener.set_nonblocking(true).expect("Count not enable non-blocking");
 
     // Further reading: https://doc.rust-lang.org/std/sync/index.html
-    let mut clients = vec![];
-    let (tx, rx) = mpsc::channel::<String>();
+    let mut clients: Vec<TcpStream> = Vec::new();
+
     loop {
-        if let Ok((mut socket, addr)) = listener.accept() { // fn accept -> Result<TcpStream, SocketAddr)
-            println!("Client {} connected", addr);
+        match listener.accept() {
+            Err(e) => println!("Client connection failed: {:?}", e),
+            Ok((mut socket, addr)) => {
+                println!("Client {} connected.", addr);
+               
+                clients.push(socket);
+                
+                let mut buffer = vec![0; 512];
+                let bytes_read = socket.read(&mut buffer);
 
-            // We clone it so that we can move it into a thread
-            let tx = tx.clone();
-            thread::spawn(move || loop {
-                // fill a vector of length MSIZE with zeros
-                let mut buff = vec![0; MSIZE];
-
-                // borrow the buffer and mutate it
-                match socket.read_exact(&mut buff) { // -> Result()
-                    Ok(_) => {
-                        // loop through the buffer, while not zero (default)
-                        // and turn it into a Vector? - Which it already is?
-                        let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-                        let msg = String::from_utf8(msg).expect("Invalid utf8");
-                    },
-                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
-                    Err(_) => {
-                        println!("Closing: {}", addr);
-                        break;
-                    }
-                }
-
-                sleep();
-
-            });
+                for client in clients { client.write(&buffer); }
+            },
         }
-
-        if let Ok(msg) = rx.try_recv() {
-            // try and recieve the messsage from the channel and if you get one,
-            // pass it into this closure func which is the logic of the filter
-
-            clients = clients.into_iter().filter_map(|mut client: TcpStream| {
-                let mut buff = msg.clone().into_bytes();
-                buff.resize(MSIZE, 0);
-
-                // What's the type of client?!
-                client.write_all(&buff).map(|_| client).ok()
-
-            }).collect::<Vec<_>>();
-        }
-
-        sleep();
     }
 }
