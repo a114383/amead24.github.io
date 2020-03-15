80f578cdbb4e3f7cac5024cbc2de1c31c8676c79
amead24 <amead2@kent.edu>
2020-03-14 20:56:29 -0400

    Created a few basic views, and plugged them into App.vue using named routes.  Was able to get npm run serve up and running and displaying a skeleton framework.

--- vue/vm2/real-world-vue-lesson4-routing-start/src/App.vue
+++ vue/vm2/real-world-vue-lesson4-routing-start/src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link :to="{ name: 'event-list' }">List</router-link>
       <router-link :to="{ name: 'event-show' }">Show Event #1</router-link>
-      <router-link :to="{ name: 'event-create' }">Create Event</router-link>
+      <router-link :to="{ name: 'event-create' }">Create an Event</router-link>
     </div>
     <router-view />
   </div>
