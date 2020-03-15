ca01b79e88bc05d12c7c1c31a453aa94a66aab0e
amead24 <amead2@kent.edu>
2020-03-14 23:47:37 -0400

    wowza... centrazlied api service oh-my!

--- vue/vm2/real-world-vue-lesson9-axios-start/src/views/EventList.vue
+++ vue/vm2/real-world-vue-lesson9-axios-start/src/views/EventList.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard/>
+    <!-- 
+      Most interestingly we're looping through each event
+      and then passing those as a prop on an individual basis
+    -->
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventServices.js'
 
 export default {
+  data() {
+    return {
+      events: []
+    }
+  },
   components: {
     EventCard
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(errors => {
+        console.log(errors)
+      })
   }
 }
 </script>
