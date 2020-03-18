5e0e8730a55792d771f5594e3082a6f3ab60393c
amead24 <amead2@kent.edu>
2020-03-17 20:35:22 -0400

    finished lesson on vuex state getters, including getters with parameters and getters calling getters.  The big idea being that if you are accessing the store in the same way from multiple different components than you can centralize those calls to the store.js file.  Lastly if you have data that does not need to be shared it can be stored inside the individual components rather than the store object

--- vue/vm3/real-world-vue-lesson11-vuex-start/src/store.js
+++ vue/vm3/real-world-vue-lesson11-vuex-start/src/store.js
@@ -4,7 +4,40 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'idk', name: 'tony mead' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    // passing getters to another getter
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
+    },
+    // passing parameters to getters
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 })
