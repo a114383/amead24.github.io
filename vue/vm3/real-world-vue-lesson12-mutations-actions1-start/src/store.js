a1cbb1fc25fbbf19ef0b1f259e2437c65654c066
amead24 <amead2@kent.edu>
2020-03-19 20:55:37 -0400

    mutations and async.  Components use actions using this..dispatch(<name>, parameters), and actions are supposed to wrap mutations using commit('<MUTATION>', parameters).  Not only does this help extend reusability, but because actions are async it helps to wrap mutation calls with then()/catch(), and then also wrap the dispatching call with then()/catch()

--- vue/vm3/real-world-vue-lesson12-mutations-actions1-start/src/store.js
+++ vue/vm3/real-world-vue-lesson12-mutations-actions1-start/src/store.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// Still not clear on "@" vs. "./"
+import EventService from '@/services/EventService.js'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -15,15 +18,27 @@ export default new Vuex.Store({
       'food',
       'community'
     ],
-    events: [
-      { id: 1, title: '...', organizer: '...' },
-      { id: 2, title: '...', organizer: '...' },
-      { id: 3, title: '...', organizer: '...' },
-      { id: 4, title: '...', organizer: '...' }
-    ]
+    events: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    // Sync way to modify the state
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    // Async way to modify the state
+    // Always put mutations within Actions, as app grows it helps avoid refactor
+    createEvent({ commit }, event) {
+      EventService.postEvent(event).then(() => {
+        // because actions are async we want to wait until the api call has finished
+        // before we update the events on the front end/vuex  state list
+        commit('ADD_EVENT', event)
+        // Then whatever trigger this event can wait as well in order
+        // to make sure that this is idempotent-ish
+      })
+    }
+  },
   getters: {
     getEventById: state => id => {
       return state.events.find(event => event.id === id)
