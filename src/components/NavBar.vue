0dda90e94a1046389ee93ad87e3bea7ce6f677a9
a114383 <A114383@progressive.com>
2020-05-27 00:38:23 +0000

    adding vue router

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="nav" class="nav">
+    <router-link to="/">Home</router-link>
+    <nav>
+      <router-link :to="{ name: 'articles' }">Articles</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import ArticleList from '../views/ArticleList.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/articles',
+    name: 'article-list',
+    component: ArticleList,
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
